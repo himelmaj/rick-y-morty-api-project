f9ecc411b1ffd50eeb1618b5f1789c1adb8df5ac
Himel Majumder <himelmajumder1@gmail.com>
2023-11-17 01:56:10 +0100

    Refactor Pagination component to hide previous button on first page

--- frontend/src/components/Pagination.jsx
+++ frontend/src/components/Pagination.jsx
@@ -1,32 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import next_icon from "../assets/next_icon.svg";
 import prev_icon from "../assets/prev_icon.svg";
-
-export default function Pagination({
-  page,
-  maxPage,
-}) {
-
+export default function Pagination({ page, maxPage }) {
   const navigate = useNavigate();
   return (
     <>
-  {page > 1 ? (
-    <button
-      className="pagination-button"
-      onClick={() => navigate(`/characters/page/${page - 1}`)}
-    >
-      <img src={next_icon} alt="Next" />
-    </button>
-  ): <div></div>}
+      {page > 1 ? (
+        <button
+          className="pagination-button"
+          onClick={() => navigate(`/characters/page/${page - 1}`)}
+        >
+          <img src={next_icon} alt="Next" />
+        </button>
+      ) : (
+        <div className="hidden md:block"></div>
+      )}
 
-  {page < maxPage && (
-    <button
-      className={"pagination-button"}
-      onClick={() => navigate(`/characters/page/${page + 1}`)}
-    >
-      <img src={prev_icon} alt="Previous" />
-    </button>
-  )}
+      {page < maxPage && (
+        <button
+          className={"pagination-button"}
+          onClick={() => navigate(`/characters/page/${page + 1}`)}
+        >
+          <img src={prev_icon} alt="Previous" />
+        </button>
+      )}
     </>
   );
 }
