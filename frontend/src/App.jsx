a1c6b89ae3be28d9e8abec5932069b07afa6b31a
MONLAU\himelmaj <himelmaj@campus.monlau.com>
2023-11-27 18:58:56 +0100

    Add Footer component to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { Locations } from "./pages/Locations";
 import { Episodes } from "./pages/Episodes";
 import { Index } from "./pages/Index";
 import { CharacterPage } from "./pages/CharacterPage";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/locations" element={<Locations />}></Route>
           <Route path="/index" element={<Index />}></Route>
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
